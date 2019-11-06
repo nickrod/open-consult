d280534ad7adde0e62e15844f0b7ca19c76de342
nickrod <nickrod@users.noreply.github.com>
2019-11-05 22:04:35 -0800

    Remove city.sh and update database.sh

--- database.sh
+++ database.sh
@@ -5,13 +5,12 @@
 DBFILE="./db.sql"
 TRIGGERFILE="./triggers.sql"
 INSERTFILE="./inserts.sql"
-CONFIGFILE="/usr/local/include/simpleconsulting.ini"
 
 # db vars
 
-USER=`awk -F"=" '/username/{gsub(/^[ \t]+/, "", $2); print $2}' $CONFIGFILE`
-DB=`awk -F"=" '/dbname/{gsub(/^[ \t]+/, "", $2); print $2}' $CONFIGFILE`
-HOST=`awk -F"=" '/host/{gsub(/^[ \t]+/, "", $2); print $2}' $CONFIGFILE`
+USER=""
+DB=""
+HOST=""
 
 # run the sql commands
 
